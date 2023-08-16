0b0d0d09bed09d1a742c5e7bd1793c9427bd3835
Rizvan <rizvan.chalilovas@proton.me>
2023-08-16 11:17:02 +0300

    setup fly.io deployment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors  = require("cors");
 
 const app = express();
 
@@ -8,6 +9,7 @@ morgan.token('body', (req, res) => {
   return stringBody === '{}' ? ' ' : stringBody;
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
@@ -94,7 +96,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server is listening on port: ${PORT}`);
 })
